c9f7edad8a82da82210a1450e0743b48101b7116
Marlus Saraiva <marlus.saraiva@gmail.com>
2017-03-23 23:20:59 -0300

    Generate, send and receive auth tokens when using tcp/ip sockets

--- test/server_test.exs
+++ test/server_test.exs
@@ -23,18 +23,20 @@ defmodule ElixirSense.ServerTest do
   import TCPHelper
 
   setup_all do
-    "ok:localhost:" <> port_or_file = capture_io(fn ->
+    ["ok", "localhost", port, auth_token] = capture_io(fn ->
       ElixirSense.Server.start(["tcpip", "0", "dev"])
-    end)
-    port_or_file = port_or_file |> String.trim |> String.to_integer
-    {:ok, socket} = :gen_tcp.connect('localhost', port_or_file, [:binary, active: false, packet: 4])
+    end) |> String.split(":")
+    port = port |> String.trim |> String.to_integer
+    auth_token = auth_token|> String.trim
+    {:ok, socket} = :gen_tcp.connect('localhost', port, [:binary, active: false, packet: 4])
 
-    {:ok, socket: socket}
+    {:ok, socket: socket, auth_token: auth_token}
   end
 
-  test "definition request", %{socket: socket} do
+  test "definition request", %{socket: socket, auth_token: auth_token} do
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "definition",
       "payload" => %{
         "buffer" => "Enum.to_list",
@@ -45,9 +47,10 @@ defmodule ElixirSense.ServerTest do
     assert send_request(socket, request) =~ "enum.ex:2523"
   end
 
-  test "signature request", %{socket: socket} do
+  test "signature request", %{socket: socket, auth_token: auth_token} do
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "signature",
       "payload" => %{
         "buffer" => "List.flatten(par, ",
@@ -58,9 +61,10 @@ defmodule ElixirSense.ServerTest do
     assert send_request(socket, request).active_param == 1
   end
 
-  test "quote request", %{socket: socket} do
+  test "quote request", %{socket: socket, auth_token: auth_token} do
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "quote",
       "payload" => %{
         "code" => "var = 1",
@@ -69,9 +73,10 @@ defmodule ElixirSense.ServerTest do
     assert send_request(socket, request) == "{:=, [line: 1], [{:var, [line: 1], nil}, 1]}"
   end
 
-  test "match request", %{socket: socket} do
+  test "match request", %{socket: socket, auth_token: auth_token} do
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "match",
       "payload" => %{
         "code" => "{var1, var2} = {1, 2}",
@@ -80,9 +85,10 @@ defmodule ElixirSense.ServerTest do
     assert send_request(socket, request) == "# Bindings\n\nvar1 = 1\n\nvar2 = 2"
   end
 
-  test "expand request", %{socket: socket} do
+  test "expand request", %{socket: socket, auth_token: auth_token} do
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "expand_full",
       "payload" => %{
         "buffer" => "",
@@ -93,9 +99,10 @@ defmodule ElixirSense.ServerTest do
     assert send_request(socket, request).expand_once == "if(true) do\n  nil\nelse\n  false\nend"
   end
 
-  test "docs request", %{socket: socket} do
+  test "docs request", %{socket: socket, auth_token: auth_token} do
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "docs",
       "payload" => %{
         "buffer" => "Enum.to_list",
@@ -106,9 +113,10 @@ defmodule ElixirSense.ServerTest do
     assert send_request(socket, request).docs.docs =~ "> Enum.to_list"
   end
 
-  test "suggestions request", %{socket: socket} do
+  test "suggestions request", %{socket: socket, auth_token: auth_token} do
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "suggestions",
       "payload" => %{
         "buffer" => "List.",
@@ -119,13 +127,14 @@ defmodule ElixirSense.ServerTest do
     assert send_request(socket, request) |> Enum.at(0) == %{type: :hint, value: "List."}
   end
 
-  test "set_context request", %{socket: socket} do
+  test "set_context request", %{socket: socket, auth_token: auth_token} do
     {_, _, _, env, cwd, _} = ContextLoader.get_state()
 
     assert env == "dev"
 
     request = %{
       "request_id" => 1,
+      "auth_token" => auth_token,
       "request" => "set_context",
       "payload" => %{
         "env" => "test",
@@ -138,6 +147,22 @@ defmodule ElixirSense.ServerTest do
     assert env == "test"
   end
 
+  test "unauthorized request", %{socket: socket} do
+    request = %{
+      "request_id" => 1,
+      "auth_token" => "not the right token",
+      "request" => "match",
+      "payload" => %{
+        "code" => "{var1, var2} = {1, 2}",
+      }
+    }
+    data = :erlang.term_to_binary(request)
+    response = send_and_recv(socket, data) |> :erlang.binary_to_term
+
+    assert response.payload == nil
+    assert response.error == "unauthorized"
+  end
+
 end
 
 defmodule ElixirSense.ServerUnixSocketTest do
@@ -147,11 +172,11 @@ defmodule ElixirSense.ServerUnixSocketTest do
   import TCPHelper
 
   setup_all do
-    "ok:localhost:" <> port_or_file = capture_io(fn ->
+    ["ok", "localhost", file] = capture_io(fn ->
        ElixirSense.Server.start(["unix", "0", "dev"])
-     end)
-    port_or_file = port_or_file |> String.trim |> String.to_charlist
-    {:ok, socket} = :gen_tcp.connect({:local, port_or_file}, 0, [:binary, active: false, packet: 4])
+     end) |> String.split(":")
+    file = file |> String.trim |> String.to_charlist
+    {:ok, socket} = :gen_tcp.connect({:local, file}, 0, [:binary, active: false, packet: 4])
 
     {:ok, socket: socket}
   end
@@ -159,6 +184,7 @@ defmodule ElixirSense.ServerUnixSocketTest do
   test "suggestions request", %{socket: socket} do
     request = %{
       "request_id" => 1,
+      "auth_token" => nil,
       "request" => "suggestions",
       "payload" => %{
         "buffer" => "List.",
