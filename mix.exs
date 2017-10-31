feab4e38787babc221f4a85af60336fddf3fc43c
Marlus Saraiva <marlus.saraiva@gmail.com>
2017-10-31 07:39:25 -0300

    Update version to 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirSense.Mixfile do
 
   def project do
     [app: :elixir_sense,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
