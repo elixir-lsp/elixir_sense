3e3b7c90882950075aaf101750788aa56e5a80ce
Marlus Saraiva <marlus.saraiva@gmail.com>
2018-02-21 23:02:51 -0300

    Fix error when parsing code using non existing modules

--- test/elixir_sense/core/parser_test.exs
+++ test/elixir_sense/core/parser_test.exs
@@ -116,4 +116,24 @@ defmodule ElixirSense.Core.ParserTest do
       }
   end
 
+  test "parse_string ignores non existing modules in `use`" do
+    source = """
+    defmodule MyModule do
+      use EnumFake
+      import List
+      
+    end
+    """
+    assert %Metadata{
+      error: nil,
+      mods_funs_to_lines: %{{MyModule, nil, nil} => %{lines: [1]}},
+      lines_to_env: %{
+        1 => %Env{imports: []},
+        3 => %Env{imports: [List]}
+      },
+      source: "defmodule MyModule" <> _
+    } = parse_string(source, true, true, 4)
+
+  end
+
 end
