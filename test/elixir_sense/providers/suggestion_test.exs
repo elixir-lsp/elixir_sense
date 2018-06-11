e87957585127dc2e96c5b7c5c71d64f55eba23f2
Marlus Saraiva <marlus.saraiva@gmail.com>
2018-06-11 19:48:30 -0300

    Fix tests due to new API

--- test/elixir_sense/providers/suggestion_test.exs
+++ test/elixir_sense/providers/suggestion_test.exs
@@ -10,7 +10,7 @@ defmodule ElixirSense.Providers.SuggestionTest do
   end
 
   test "find definition of functions from Kernel" do
-    result = Suggestion.find("List", [], [], [], [], [], SomeModule) |> Enum.take(16)
+    result = Suggestion.find("List", [], [], [], [], [], SomeModule, "") |> Enum.take(16)
     assert result |> Enum.at(0) == %{type: :hint, value: "List."}
     assert result |> Enum.at(1) == %{name: "List", subtype: nil, summary: "Functions that work on (linked) lists.", type: :module}
     assert result |> Enum.at(3) == %{args: "", arity: 1, name: "__info__", origin: "List", spec: nil, summary: "", type: "function"}
@@ -21,7 +21,7 @@ defmodule ElixirSense.Providers.SuggestionTest do
   end
 
   test "return completion candidates for 'Str'" do
-    assert Suggestion.find("Str", [], [], [], [], [], SomeModule) == [
+    assert Suggestion.find("Str", [], [], [], [], [], SomeModule, "") == [
       %{type: :hint, value: "Str"},
       %{name: "Stream", subtype: :struct, summary: "Module for creating and composing streams.", type: :module},
       %{name: "String", subtype: nil, summary: "A String in Elixir is a UTF-8 encoded binary.", type: :module},
@@ -34,7 +34,7 @@ defmodule ElixirSense.Providers.SuggestionTest do
       %{type: :hint, value: "List.delete"},
       %{args: "list,item", arity: 2, name: "delete", origin: "List", spec: "@spec delete(list, any) :: list", summary: "Deletes the given" <> _, type: "function"},
       %{args: "list,index", arity: 2, name: "delete_at", origin: "List", spec: "@spec delete_at(list, integer) :: list", summary: "Produces a new list by " <> _, type: "function"}
-    ] = Suggestion.find("List.del", [], [], [], [], [], SomeModule)
+    ] = Suggestion.find("List.del", [], [], [], [], [], SomeModule, "")
   end
 
   test "return completion candidates for module with alias" do
@@ -42,11 +42,11 @@ defmodule ElixirSense.Providers.SuggestionTest do
       %{type: :hint, value: "MyList.delete"},
       %{args: "list,item", arity: 2, name: "delete", origin: "List", spec: "@spec delete(list, any) :: list", summary: "Deletes the given " <> _, type: "function"},
       %{args: "list,index", arity: 2, name: "delete_at", origin: "List", spec: "@spec delete_at(list, integer) :: list", summary: "Produces a new list " <> _, type: "function"}
-    ] = Suggestion.find("MyList.del", [], [{MyList, List}], [], [], [], SomeModule)
+    ] = Suggestion.find("MyList.del", [], [{MyList, List}], [], [], [], SomeModule, "")
   end
 
   test "return completion candidates for functions from import" do
-    assert Suggestion.find("say", [MyModule], [], [], [], [], SomeModule) == [
+    assert Suggestion.find("say", [MyModule], [], [], [], [], SomeModule, "") == [
       %{type: :hint, value: "say"},
       %{args: "", arity: 0, name: "say_hi", origin: "ElixirSense.Providers.SuggestionTest.MyModule", spec: "", summary: "", type: "public_function"}
     ]
