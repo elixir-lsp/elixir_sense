3e30007f306160007a4fb8ad8e5f20e7be81bfca
Aldric Giacomoni <trevoke@gmail.com>
2018-11-07 09:49:58 -0500

    continue refactoring

--- lib/elixir_sense/core/introspection.ex
+++ lib/elixir_sense/core/introspection.ex
@@ -34,43 +34,46 @@ defmodule ElixirSense.Core.Introspection do
   defp do_something_you_can_do_with_fetch_docs(module, category) do
     case Code.fetch_docs(module) do
       {:docs_v1, moduledoc_line, _beam_language, "text/markdown", moduledoc, _metadata, docs} ->
-        docs = Enum.map(docs, &to_old_format/1)
-
-        case category do
-          :moduledoc ->
-            moduledoc_en =
-              case moduledoc do
-                %{"en" => moduledoc_en} -> moduledoc_en
-                false -> false
-                _ -> nil
-              end
-
-            case {moduledoc_line, moduledoc_en} do
-              {_, nil} -> nil
-              {nil, _} -> nil
-              _ -> {moduledoc_line, moduledoc_en}
-            end
-
-          :docs ->
-            Enum.filter(docs, &match?({_, _, def_type, _, _} when def_type in [:def, :defmacro], &1))
-
-          :callback_docs ->
-            Enum.filter(
-              docs,
-              &match?({_, _, kind, _} when kind in [:callback, :macrocallback], &1)
-            )
-
-          :type_docs ->
-            Enum.filter(docs, &match?({_, _, :type, _}, &1))
-
-          :all ->
-            [:moduledoc, :docs, :callback_docs, :type_docs]
-            |> Enum.map(&{&1, get_docs(module, &1)})
-        end
-
+        parse_docs_v1(docs, category, moduledoc, moduledoc_line, module)
       _ ->
         nil
     end
+  end
+
+  defp parse_docs_v1(docs, category, moduledoc, moduledoc_line, module) do
+    docs = Enum.map(docs, &to_old_format/1)
+    case category do
+      :moduledoc ->
+        moduledoc_en =
+          case moduledoc do
+            %{"en" => moduledoc_en} -> moduledoc_en
+            false -> false
+            _ -> nil
+          end
+
+        case {moduledoc_line, moduledoc_en} do
+          {_, nil} -> nil
+          {nil, _} -> nil
+          _ -> {moduledoc_line, moduledoc_en}
+        end
+
+      :docs ->
+        Enum.filter(docs, &match?({_, _, def_type, _, _} when def_type in [:def, :defmacro], &1))
+
+      :callback_docs ->
+        Enum.filter(
+          docs,
+          &match?({_, _, kind, _} when kind in [:callback, :macrocallback], &1)
+        )
+
+      :type_docs ->
+        Enum.filter(docs, &match?({_, _, :type, _}, &1))
+
+      :all ->
+        [:moduledoc, :docs, :callback_docs, :type_docs]
+        |> Enum.map(&{&1, get_docs(module, &1)})
+    end
+
   end
 
   def all_modules do
