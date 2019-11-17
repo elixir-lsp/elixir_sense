ebc5ca593e4310573e3a6b3605c863bdae0c0ed5
Lukasz Samson <lukaszsamson@gmail.com>
2019-11-17 19:23:22 +0100

    fix a flaky test sometimes failing due to ExCoveralls module beeing loaded

--- test/alchemist/helpers/complete_test.exs
+++ test/alchemist/helpers/complete_test.exs
@@ -62,7 +62,9 @@ defmodule Alchemist.Helpers.CompleteTest do
             ]} = expand('Ma')
 
     assert {:yes, 't', [%{name: "Dict"}]} = expand('Dic')
-    assert {:yes, [], [%{name: "ExUnit"}, %{name: "Exception"}]} = expand('Ex')
+    assert {:yes, [], suggestions} = expand('Ex')
+    assert Enum.any?(suggestions, & &1.name == "ExUnit")
+    assert Enum.any?(suggestions, & &1.name == "Exception")
   end
 
   test "Elixir no completion for underscored functions with no doc" do
