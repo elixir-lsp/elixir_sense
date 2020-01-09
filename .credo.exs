3b55931d635ed6f43768ffe02ed7a79d50e700f1
Victor Rodrigues <victorc.rodrigues@gmail.com>
2020-01-09 23:22:15 +0100

    Relax for now max nesting from 3 to 4

--- .credo.exs
+++ .credo.exs
@@ -111,7 +111,7 @@
         {Credo.Check.Refactor.MatchInCondition, []},
         {Credo.Check.Refactor.NegatedConditionsInUnless, []},
         {Credo.Check.Refactor.NegatedConditionsWithElse, []},
-        {Credo.Check.Refactor.Nesting, max_nesting: 3},
+        {Credo.Check.Refactor.Nesting, max_nesting: 4},
         {Credo.Check.Refactor.UnlessWithElse, []},
         {Credo.Check.Refactor.WithClauses, []},
 
