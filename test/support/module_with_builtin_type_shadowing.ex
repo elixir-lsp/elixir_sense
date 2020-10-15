e291b52f498133d558c924a0ceea0abcc3b4818b
Adriano Mitre <amitre@xerpa.com.br>
2020-10-15 12:37:36 -0300

    Revert "enable --warnings-as-errors on CI"
    This reverts commit 4dc5e661d33a20b3bfb866598402da474b8aeb4a.

--- test/support/module_with_builtin_type_shadowing.ex
+++ test/support/module_with_builtin_type_shadowing.ex
@@ -1,9 +1,6 @@
-# TODO: revert this commit when "warnings-as-errors: true" on CI moves to Elixir 1.11
-if Version.match?(System.build_info().version, ">= 1.10.0") do
-  defmodule ElixirSenseExample.ModuleWithBuiltinTypeShadowing do
-    @compile {:no_warn_undefined, {B.Callee, :fun, 0}}
-    def plain_fun do
-      B.Callee.fun()
-    end
+defmodule ElixirSenseExample.ModuleWithBuiltinTypeShadowing do
+  @compile {:no_warn_undefined, {B.Callee, :fun, 0}}
+  def plain_fun do
+    B.Callee.fun()
   end
 end
