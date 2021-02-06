0e8477b6db730656dd7ba9f086d329a3a344b14d
Po Chen <chenpaul914@gmail.com>
2021-02-06 23:46:37 +1100

    require first char to match if provided

--- lib/elixir_sense/providers/suggestion/matcher.ex
+++ lib/elixir_sense/providers/suggestion/matcher.ex
@@ -26,7 +26,7 @@ defmodule ElixirSense.Providers.Suggestion.Matcher do
       true
 
       iex> ElixirSense.Providers.Suggestion.Matcher.match?("map", "ap")
-      true
+      false
 
       iex> ElixirSense.Providers.Suggestion.Matcher.match?("", "")
       true
@@ -38,23 +38,35 @@ defmodule ElixirSense.Providers.Suggestion.Matcher do
       false
   """
   @spec match?(name :: String.t(), hint :: String.t()) :: boolean()
-  def match?(<<head::utf8, name_rest::binary>>, <<head::utf8, hint_rest::binary>>) do
-    match?(name_rest, hint_rest)
+  def match?(<<name_head::utf8, _name_rest::binary>>, <<hint_head::utf8, _hint_rest::binary>>)
+      when name_head != hint_head do
+    false
   end
 
-  def match?(<<_head::utf8, name_rest::binary>>, <<_not_head::utf8, _hint_rest::binary>> = hint) do
-    match?(name_rest, hint)
+  def match?(name, hint) do
+    do_match?(name, hint)
   end
 
-  def match?(_name_rest, <<>>) do
+  defp do_match?(<<head::utf8, name_rest::binary>>, <<head::utf8, hint_rest::binary>>) do
+    do_match?(name_rest, hint_rest)
+  end
+
+  defp do_match?(
+         <<_head::utf8, name_rest::binary>>,
+         <<_not_head::utf8, _hint_rest::binary>> = hint
+       ) do
+    do_match?(name_rest, hint)
+  end
+
+  defp do_match?(_name_rest, <<>>) do
     true
   end
 
-  def match?(<<>>, <<>>) do
+  defp do_match?(<<>>, <<>>) do
     true
   end
 
-  def match?(<<>>, _) do
+  defp do_match?(<<>>, _) do
     false
   end
 end
