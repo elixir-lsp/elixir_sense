5c6d4703b3c413aad4ae485442803ba16e4b1efe
Ashton Wiersdorf <ashton.wiersdorf@spiff.com>
2021-06-11 14:44:13 -0600

    Add clause for only %{doc: ...} being returned from type info

--- lib/elixir_sense/core/type_info.ex
+++ lib/elixir_sense/core/type_info.ex
@@ -113,6 +113,7 @@ defmodule ElixirSense.Core.TypeInfo do
             case ti do
               %{spec: ast} -> spec_ast_to_string(ast)
               %{signature: signature} -> signature
+              %{doc: doc} -> doc
               nil -> "#{type}"
             end
       }
