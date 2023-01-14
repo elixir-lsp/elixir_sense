a0213a629613aad07e301e2bd11e361432ba44c9
Lukasz Samson <lukaszsamson@gmail.com>
2023-01-14 17:11:55 +0100

    fix credo issues

--- .credo.exs
+++ .credo.exs
@@ -105,7 +105,7 @@
         #
         {Credo.Check.Refactor.CondStatements, []},
         {Credo.Check.Refactor.CyclomaticComplexity, false},
-        {Credo.Check.Refactor.FunctionArity, max_arity: 7},
+        {Credo.Check.Refactor.FunctionArity, max_arity: 10},
         {Credo.Check.Refactor.LongQuoteBlocks, []},
         {Credo.Check.Refactor.MapInto, false},
         {Credo.Check.Refactor.MatchInCondition, []},
