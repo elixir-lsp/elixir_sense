686c1b6ed6c05d1169b940157ae099269f9f032b
Sam Wight <me@samw.dev>
2023-05-02 16:22:44 -0500

    Use Elixir-provided list of module attributes.

--- lib/elixir_sense/core/builtin_attributes.ex
+++ lib/elixir_sense/core/builtin_attributes.ex
@@ -1,34 +1,7 @@
 defmodule ElixirSense.Core.BuiltinAttributes do
   @moduledoc false
-  @list ~w(
-    optional_callbacks
-    behaviour
-    impl
-    derive
-    enforce_keys
-    struct
-    compile
-    deprecated
-    dialyzer
-    file
-    external_resource
-    on_load
-    on_definition
-    vsn
-    after_compile
-    after_verify
-    before_compile
-    fallback_to_any
-    type
-    typep
-    opaque
-    spec
-    callback
-    macrocallback
-    typedoc
-    doc
-    moduledoc
-  )a
+
+  @list Map.keys(Module.reserved_attributes())
 
   def all, do: @list
 end
