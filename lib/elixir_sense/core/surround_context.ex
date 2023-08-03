4160e1a321f2330c07e20b6cca71d095e421d292
Lukasz Samson <lukaszsamson@gmail.com>
2023-08-03 08:25:35 +0200

    fix some rare crashes
    cover all surround context variants as of elixir 1.15

--- lib/elixir_sense/core/surround_context.ex
+++ lib/elixir_sense/core/surround_context.ex
@@ -2,10 +2,21 @@ defmodule ElixirSense.Core.SurroundContext do
   @moduledoc false
   alias ElixirSense.Core.Introspection
 
-  # TODO structs from 1.14
   def to_binding({:alias, charlist}, _current_module) do
     {{:atom, :"Elixir.#{charlist}"}, nil}
   end
+
+  # do not handle any other local_or_var
+  def to_binding({:alias, {:local_or_var, ~c"__MODULE__"}, charlist}, current_module) do
+    if current_module not in [nil, Elixir] do
+      {{:atom, :"#{current_module}.#{charlist}"}, nil}
+    end
+  end
+
+  def to_binding({:alias, {:local_or_var, _charlist1}, _charlist}), do: nil
+
+  # TODO handle this case?
+  def to_binding({:alias, {:module_attribute, _charlist1}, _charlist}), do: nil
 
   def to_binding({:dot, inside_dot, charlist}, current_module) do
     {inside_dot_to_binding(inside_dot, current_module), :"#{charlist}"}
@@ -14,12 +25,6 @@ defmodule ElixirSense.Core.SurroundContext do
   def to_binding({:local_or_var, ~c"__MODULE__"}, current_module) do
     if current_module not in [nil, Elixir] do
       {{:atom, current_module}, nil}
-    end
-  end
-
-  def to_binding({:alias, {:local_or_var, ~c"__MODULE__"}, charlist}, current_module) do
-    if current_module not in [nil, Elixir] do
-      {{:atom, :"#{current_module}.#{charlist}"}, nil}
     end
   end
 
@@ -43,10 +48,6 @@ defmodule ElixirSense.Core.SurroundContext do
     {nil, :"#{charlist}"}
   end
 
-  def to_binding({:unquoted_atom, charlist}, _current_module) do
-    {{:atom, :"#{charlist}"}, nil}
-  end
-
   def to_binding({:sigil, charlist}, _current_module) do
     {nil, :"sigil_#{charlist}"}
   end
@@ -55,20 +56,21 @@ defmodule ElixirSense.Core.SurroundContext do
     {{:atom, :"Elixir.#{charlist}"}, nil}
   end
 
+  # handles
+  # {:alias, inside_alias, charlist}
+  # {:local_or_var, charlist}
+  # {:module_attribute, charlist}
+  # {:dot, inside_dot, charlist}
+  def to_binding({:struct, inside_struct}, current_module) do
+    to_binding(inside_struct, current_module)
+  end
+
+  def to_binding({:unquoted_atom, charlist}, _current_module) do
+    {{:atom, :"#{charlist}"}, nil}
+  end
+
   def to_binding({:keyword, charlist}, _current_module) do
     {:keyword, :"#{charlist}"}
-  end
-
-  defp inside_dot_to_binding({:module_attribute, inside_charlist}, _current_module) do
-    {:attribute, :"#{inside_charlist}"}
-  end
-
-  defp inside_dot_to_binding({:var, inside_charlist}, _current_module) do
-    {:variable, :"#{inside_charlist}"}
-  end
-
-  defp inside_dot_to_binding({:unquoted_atom, inside_charlist}, _current_module) do
-    {:atom, :"#{inside_charlist}"}
   end
 
   defp inside_dot_to_binding({:alias, inside_charlist}, _current_module)
@@ -85,13 +87,25 @@ defmodule ElixirSense.Core.SurroundContext do
     end
   end
 
-  # TODO attr?
+  # TODO handle {:alias, {:module_attribute, charlist1}, charlist}?
   defp inside_dot_to_binding({:alias, _other, _inside_charlist}, _current_module) do
     nil
   end
 
   defp inside_dot_to_binding({:dot, inside_dot, inside_charlist}, current_module) do
     {:call, inside_dot_to_binding(inside_dot, current_module), :"#{inside_charlist}", []}
+  end
+
+  defp inside_dot_to_binding({:module_attribute, inside_charlist}, _current_module) do
+    {:attribute, :"#{inside_charlist}"}
+  end
+
+  defp inside_dot_to_binding({:unquoted_atom, inside_charlist}, _current_module) do
+    {:atom, :"#{inside_charlist}"}
+  end
+
+  defp inside_dot_to_binding({:var, inside_charlist}, _current_module) do
+    {:variable, :"#{inside_charlist}"}
   end
 
   def expand({{:atom, module}, func}, aliases) do
