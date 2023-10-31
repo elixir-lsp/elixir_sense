6b4c0d0d7e078b669fabc84230a974f05032daa3
goose <purification1997@gmail.com>
2023-10-31 21:07:44 +0700

    Split guard type infer logic into separate module

--- lib/elixir_sense/core/metadata_builder.ex
+++ lib/elixir_sense/core/metadata_builder.ex
@@ -13,7 +13,7 @@ defmodule ElixirSense.Core.MetadataBuilder do
   alias ElixirSense.Core.State
   alias ElixirSense.Core.State.VarInfo
   alias ElixirSense.Core.TypeInfo
-  alias ElixirSense.Core.Binding
+  alias ElixirSense.Core.Guard
 
   @scope_keywords [:for, :fn, :with]
   @block_keywords [:do, :else, :rescue, :catch, :after]
@@ -1670,7 +1670,7 @@ defmodule ElixirSense.Core.MetadataBuilder do
   end
 
   def infer_type_from_guards(guard_ast, vars, state) do
-    type_info = type_information_from_guards(guard_ast, state)
+    type_info = Guard.type_information_from_guards(guard_ast, state)
 
     Enum.reduce(type_info, vars, fn {var, type}, acc ->
       index = Enum.find_index(acc, &(&1.name == var))
@@ -1680,134 +1680,6 @@ defmodule ElixirSense.Core.MetadataBuilder do
         else: acc
     end)
   end
-
-  # A guard expression can be in either these form:
-  #        :and                          :or
-  #      /     \            or          /   \              or      guard_expr
-  # guard_expr  guard_expr        guard_expr  guard_expr
-  #
-  # type information from :and subtrees are mergeable
-  # type information from :or subtrees are discarded
-  defp type_information_from_guards({:and, _, [guard_l, guard_r]}, state) do
-    left = type_information_from_guards(guard_l, state)
-    right = type_information_from_guards(guard_r, state)
-    Keyword.merge(left, right, fn _k, v1, v2 -> {:intersection, [v1, v2]} end)
-  end
-
-  defp type_information_from_guards({:or, _, [guard_l, guard_r]}, state) do
-    left = type_information_from_guards(guard_l, state)
-    right = type_information_from_guards(guard_r, state)
-
-    Keyword.merge(left, right, fn _k, v1, v2 ->
-      case {v1, v2} do
-        {{:union, types_1}, {:union, types_2}} -> {:union, types_1 ++ types_2}
-        {{:union, types}, _} -> {:union, types ++ [v2]}
-        {_, {:union, types}} -> {:union, [v1 | types]}
-        _ -> {:union, [v1, v2]}
-      end
-    end)
-  end
-
-  defp type_information_from_guards(guard_ast, state) do
-    {_, acc} =
-      Macro.prewalk(guard_ast, [], fn
-        # Standalone variable: func my_func(x) when x
-        {var, _, nil} = node, acc ->
-          {node, [{var, :boolean} | acc]}
-
-        {guard_predicate, _, params} = node, acc ->
-          case guard_predicate_type(guard_predicate, params, state) do
-            {type, binding} ->
-              {var, _, nil} = binding
-              # If we found the predicate type, we can prematurely exit traversing the subtree
-              {[], [{var, type} | acc]}
-
-            nil ->
-              {node, acc}
-          end
-
-        node, acc ->
-          {node, acc}
-      end)
-
-    acc
-  end
-
-  defp guard_predicate_type(p, params, _)
-       when p in [:is_number, :is_float, :is_integer, :round, :trunc, :div, :rem, :abs],
-       do: {:number, hd(params)}
-
-  defp guard_predicate_type(p, params, _) when p in [:is_binary, :binary_part],
-    do: {:binary, hd(params)}
-
-  defp guard_predicate_type(p, params, _) when p in [:is_bitstring, :bit_size, :byte_size],
-    do: {:bitstring, hd(params)}
-
-  defp guard_predicate_type(p, params, _) when p in [:is_list, :length], do: {:list, hd(params)}
-
-  defp guard_predicate_type(p, params, _) when p in [:hd, :tl],
-    do: {{:list, :boolean}, hd(params)}
-
-  # when hd(x) == 1
-  # when tl(x) <= 2
-  defp guard_predicate_type(p, [{guard, _, guard_params}, rhs], _)
-       when p in [:==, :===, :>=, :>, :<=, :<] and guard in [:hd, :tl] do
-    rhs_type =
-      cond do
-        is_number(rhs) -> :number
-        is_binary(rhs) -> :binary
-        is_bitstring(rhs) -> :bitstring
-        is_atom(rhs) -> :atom
-        is_boolean(rhs) -> :boolean
-        true -> nil
-      end
-
-    rhs_type = if rhs_type, do: {:list, rhs_type}, else: :list
-
-    {rhs_type, hd(guard_params)}
-  end
-
-  defp guard_predicate_type(p, params, _) when p in [:is_tuple, :elem],
-    do: {:tuple, hd(params)}
-
-  # when tuple_size(x) == 1
-  # when tuple_size(x) == 2
-  defp guard_predicate_type(p, [{:tuple_size, _, guard_params}, size], _)
-       when p in [:==, :===] do
-    type =
-      if is_integer(size) do
-        {:tuple, size, []}
-      else
-        :tuple
-      end
-
-    {type, hd(guard_params)}
-  end
-
-  defp guard_predicate_type(:is_map, params, _), do: {{:map, [], nil}, hd(params)}
-  defp guard_predicate_type(:map_size, params, _), do: {{:map, [], nil}, hd(params)}
-
-  defp guard_predicate_type(:is_map_key, [var, key], state) do
-    type =
-      case get_binding_type(state, key) do
-        {:atom, key} -> {:map, [{key, nil}], nil}
-        nil when is_binary(key) -> {:map, [{key, nil}], nil}
-        _ -> {:map, [], nil}
-      end
-
-    {type, var}
-  end
-
-  defp guard_predicate_type(:is_atom, params, _), do: {:atom, hd(params)}
-  defp guard_predicate_type(:is_boolean, params, _), do: {:boolean, hd(params)}
-
-  defp guard_predicate_type(:is_struct, [var, {:__aliases__, _, list}], state) do
-    type = {:struct, [], {:atom, expand_alias(state, list)}, nil}
-    {type, var}
-  end
-
-  defp guard_predicate_type(:is_struct, params, _), do: {{:struct, [], nil, nil}, hd(params)}
-  defp guard_predicate_type(_, _, _), do: nil
 
   # struct or struct update
   def get_binding_type(
