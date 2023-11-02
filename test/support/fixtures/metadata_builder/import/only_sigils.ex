c9a5162b78e68aeabc19c17b23a698f708f9b1ee
Lukasz Samson <lukaszsamson@gmail.com>
2023-11-02 16:18:41 +0100

    another attempt

--- test/support/fixtures/metadata_builder/import/only_sigils.ex
+++ test/support/fixtures/metadata_builder/import/only_sigils.ex
@@ -1,8 +1,13 @@
-defmodule ElixirSenseExample.Fixtures.MetadataBuilder.Import.ImportOnlySigils do
-  if Version.match?(System.version(), ">= 1.13.0") do
+if Version.match?(System.version(), ">= 1.13.0") do
+  defmodule ElixirSenseExample.Fixtures.MetadataBuilder.Import.ImportOnlySigils do
     import ElixirSenseExample.Fixtures.MetadataBuilder.Imported, only: :sigils
-  end
 
-  @env __ENV__
-  def env, do: @env
+    @env __ENV__
+    def env, do: @env
+  end
+else
+  defmodule ElixirSenseExample.Fixtures.MetadataBuilder.Import.ImportOnlySigils do
+    @env __ENV__
+    def env, do: @env
+  end
 end
