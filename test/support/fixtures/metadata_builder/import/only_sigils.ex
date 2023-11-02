ab802287c3860cbb9c5604acae422693920bcb54
Lukasz Samson <lukaszsamson@gmail.com>
2023-11-02 16:03:32 +0100

    fix build

--- test/support/fixtures/metadata_builder/import/only_sigils.ex
+++ test/support/fixtures/metadata_builder/import/only_sigils.ex
@@ -1,5 +1,8 @@
 defmodule ElixirSenseExample.Fixtures.MetadataBuilder.Import.ImportOnlySigils do
-  import ElixirSenseExample.Fixtures.MetadataBuilder.Imported, only: :sigils
+  if Version.match?(System.version(), ">= 1.13.0") do
+    import ElixirSenseExample.Fixtures.MetadataBuilder.Imported, only: :sigils
+  end
+
   @env __ENV__
   def env, do: @env
 end
