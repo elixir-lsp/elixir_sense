3d7a3eac952ca8bcc256ebb999e7007794020f3a
Lukasz Samson <lukaszsamson@gmail.com>
2024-10-30 20:10:42 +0100

    adapt to changed api on 1.18

--- lib/elixir_sense/core/compiler/rewrite.ex
+++ lib/elixir_sense/core/compiler/rewrite.ex
@@ -11,8 +11,12 @@ defmodule ElixirSense.Core.Compiler.Rewrite do
 
   defp do_rewrite(_, :erlang, _, :-, _, [arg], _s) when is_number(arg), do: {:ok, -arg}
 
-  defp do_rewrite(:match, receiver, dot_meta, right, meta, e_args, _s) do
-    :elixir_rewrite.match_rewrite(receiver, dot_meta, right, meta, e_args)
+  defp do_rewrite(:match, receiver, dot_meta, right, meta, e_args, s) do
+    if function_exported?(:elixir_rewrite, :match_rewrite, 5) do
+      :elixir_rewrite.match_rewrite(receiver, dot_meta, right, meta, e_args)
+    else
+      :elixir_rewrite.match(receiver, dot_meta, right, meta, e_args, s)
+    end
   end
 
   if Version.match?(System.version(), "< 1.14.0") do
@@ -20,9 +24,13 @@ defmodule ElixirSense.Core.Compiler.Rewrite do
       :elixir_rewrite.guard_rewrite(receiver, dot_meta, right, meta, e_args)
     end
   else
-    defp do_rewrite(:guard, receiver, dot_meta, right, meta, e_args, _s) do
-      # elixir uses guard context for error messages
-      :elixir_rewrite.guard_rewrite(receiver, dot_meta, right, meta, e_args, "guard")
+    defp do_rewrite(:guard, receiver, dot_meta, right, meta, e_args, s) do
+      if function_exported?(:elixir_rewrite, :match_rewrite, 5) do
+        # elixir uses guard context for error messages
+        :elixir_rewrite.guard_rewrite(receiver, dot_meta, right, meta, e_args, "guard")
+      else
+        :elixir_rewrite.guard(receiver, dot_meta, right, meta, e_args, s)
+      end
     end
   end
 
