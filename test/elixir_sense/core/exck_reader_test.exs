74a308f70dd89dd7b45b6e1bc95e48e6e85ddb02
Lukasz Samson <lukaszsamson@gmail.com>
2025-09-27 23:47:00 +0200

    exck reader

---
+++ test/elixir_sense/core/exck_reader_test.exs
@@ -0,0 +1,61 @@
+defmodule ElixirSense.Core.ExCkReaderTest do
+  use ExUnit.Case, async: true
+
+  alias ElixirSense.Core.ExCkReader
+
+  @table ElixirSense.Core.ExCkReader
+
+  setup do
+    if :ets.info(@table) != :undefined do
+      :ets.delete_all_objects(@table)
+    end
+
+    on_exit(fn ->
+      Application.delete_env(:elixir_sense, :exck_cache_ttl)
+
+      if :ets.info(@table) != :undefined do
+        :ets.delete_all_objects(@table)
+      end
+    end)
+
+    :ok
+  end
+
+  describe "read_chunk/1" do
+    test "returns signatures for Enum" do
+      assert {:ok, signatures} = ExCkReader.read_chunk(Enum)
+      assert is_map(signatures)
+      assert Map.has_key?(signatures, {:map, 2})
+    end
+
+    test "returns error when module has no ExCk chunk" do
+      assert {:error, :invalid_beam} = ExCkReader.read_chunk(:lists)
+    end
+
+    test "handles invalid chunk gracefully" do
+      assert {:error, :invalid_chunk} = ExCkReader.read_chunk(:fake, chunk: <<1, 2, 3>>)
+    end
+  end
+
+  test "lookup_signature returns cached entries" do
+    Application.put_env(:elixir_sense, :exck_cache_ttl, 60_000)
+
+    assert {:ok, info} = ExCkReader.lookup_signature(Enum, :map, 2)
+    assert is_map(info)
+    assert Map.has_key?(info, :sig)
+  end
+
+  test "cache invalidation honours TTL" do
+    Application.put_env(:elixir_sense, :exck_cache_ttl, 0)
+
+    assert {:ok, signatures} = ExCkReader.read_chunk(Enum)
+    assert map_size(signatures) > 0
+
+    stale = {:ok, %{foo: :bar}}
+    :ets.insert(@table, {Enum, stale, 0})
+
+    assert {:ok, refreshed} = ExCkReader.read_chunk(Enum)
+    assert refreshed != stale
+    assert map_size(refreshed) > 0
+  end
+end
