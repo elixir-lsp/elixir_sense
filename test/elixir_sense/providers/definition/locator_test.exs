54df91cc84aa0b809d66f450b71fedd5e840d49b
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-11-21 22:45:37 +0100

    Support finding function definition defined in __using__ macro
    This adds heuristic to find the function source location, even if it's
    defined insige the `__using__` macro. It supports the following cases:
    
    1. When current module has `use OtherMod` and the function is defined in
    `OtherMod`'s `__using__` - it correctly points to a relevant line in
    `OtherMod`.
    
    2. When `MyMod` has `use OtherMod` (as above) and `YetAnotherMod` calls
    `MyMod.function_defined_in_using`. This basically covers things like
    `MyApp.Repo.all`, where `Repo` has `use Ecto.Repo` - now ElixirSense
    correctly finds the definition of `all` inside `Ecto.Repo`.
    
    This is not a perfect solution, as it uses regular expressions
    and heuristic for find the definition and it might something be wrong.
    However, this covers quite important use cases in language servers, so
    in my opinion it's worth having, even if it's not 100% precise.

---
+++ test/elixir_sense/providers/definition/locator_test.exs
@@ -0,0 +1,56 @@
+defmodule ElixirSense.Providers.Definition.LocatorTest.MyBehaviour do
+  defmacro __using__(_opts) do
+    quote do
+      def my_function(), do: :ok
+    end
+  end
+end
+
+defmodule ElixirSense.Providers.Definition.LocatorTest.ModUsingBehaviour do
+  use ElixirSense.Providers.Definition.LocatorTest.MyBehaviour
+end
+
+defmodule ElixirSense.Providers.Definition.LocatorTest do
+  use ExUnit.Case, async: true
+  alias ElixirSense.Providers.Definition.Locator
+
+  test "finds definition of function defined in __using__ macro" do
+    code = """
+    defmodule MyModule do
+      use ElixirSense.Providers.Definition.LocatorTest.MyBehaviour
+
+      def test do
+        my_function()
+      end
+    end
+    """
+
+    {line, column} = {5, 5}
+
+    location = Locator.definition(code, line, column)
+
+    assert location != nil
+    assert location.type == :function
+    assert location.line == 4
+    assert location.column == 11
+  end
+
+  test "finds definition of function defined in __using__ macro via another module" do
+    code = """
+    defmodule MyModule do
+      def test do
+        ElixirSense.Providers.Definition.LocatorTest.ModUsingBehaviour.my_function()
+      end
+    end
+    """
+
+    {line, column} = {3, 70}
+
+    location = Locator.definition(code, line, column)
+
+    assert location != nil
+    assert location.type == :function
+    assert location.line == 4
+    assert location.column == 11
+  end
+end
